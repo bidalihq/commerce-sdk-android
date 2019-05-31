ce9ccb0183f6f81c1956690701cdd43e4eab4f1f
Cory Smith <cory.m.smith@gmail.com>
2019-05-31 15:01:17 -0600

    Adding close button while loading

--- library/src/main/kotlin/com/bidali/commerce/BidaliSDK.kt
+++ library/src/main/kotlin/com/bidali/commerce/BidaliSDK.kt
@@ -1,13 +1,19 @@
 package com.bidali.commerce
 
+import android.content.res.Resources
+
 import android.annotation.TargetApi
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.util.Log
+import android.util.TypedValue
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.*
+import android.widget.Button
 import android.widget.RelativeLayout
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
@@ -16,10 +22,17 @@ import org.json.JSONObject
 import wendu.webviewjavascriptbridge.WVJBWebView
 import java.math.BigDecimal
 
+fun Int.toDp(): Int = (this / Resources.getSystem().displayMetrics.density).toInt()
+fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+fun Float.toDp(): Float = (this / Resources.getSystem().displayMetrics.density)
+fun Float.toPx(): Float = (this * Resources.getSystem().displayMetrics.density)
+
 class BidaliSDK(private val context: Context) {
     private val loadingHtmlString = "<html><head><style type='text/css'>html, body {height: 100%;background: #f8f8fc;left: 0;margin: 0;overflow: hidden;text-align: center;top: 0;width: 100%;}.spinner { height: 60px;max-height: 60vmin;max-width: 60vmin;left: 50%;position: absolute;top: 50%;transform: translateX(-50%) translateY(-50%);width: 60px;z-index: 10;} .spinner .loader { animation: rotation .7s infinite linear;border: 3px solid rgba(0, 0, 0, .15);border-top-color: #4B4DF1;border-radius: 100%;box-sizing: border-box;height: 100%;width: 100%;}@keyframes rotation {from { transform: rotate(0deg); }to { transform: rotate(359deg); }}</style></head><body><div class=\"spinner\"><div class=\"loader\"></div></div></html>"
     private val tag = "BidaliSDK"
     private lateinit var dialog: Dialog
+    private lateinit var closeButton: Button
     private lateinit var webView: WVJBWebView
     private lateinit var loadingWebView: WebView
 
@@ -37,6 +50,23 @@ class BidaliSDK(private val context: Context) {
         val layoutParams = RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
         layout.addView(webView, layoutParams)
         layout.addView(loadingWebView, layoutParams)
+
+        closeButton = Button(context)
+        val buttonLayoutParams = RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        buttonLayoutParams.width = 40.toPx()
+        buttonLayoutParams.height = 40.toPx()
+        closeButton.layoutParams = buttonLayoutParams
+        closeButton.text = "✕"
+        closeButton.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18F)
+        closeButton.setOnClickListener {
+            dialog.dismiss()
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            closeButton.stateListAnimator = null
+        }
+        closeButton.setBackgroundColor(Color.TRANSPARENT)
+        closeButton.setTextColor(Color.BLACK)
+        layout.addView(closeButton)
 
         dialog = Dialog(context, android.R.style.Theme_Black_NoTitleBar_Fullscreen)
         dialog.setContentView(layout)
@@ -103,6 +133,7 @@ class BidaliSDK(private val context: Context) {
                         Log.d(tag, "onPageFinished:$url")
                         webView.visibility = View.VISIBLE
                         loadingWebView.visibility = View.GONE
+                        closeButton.visibility = View.GONE
                         super.onPageFinished(view, url)
                     }
 
